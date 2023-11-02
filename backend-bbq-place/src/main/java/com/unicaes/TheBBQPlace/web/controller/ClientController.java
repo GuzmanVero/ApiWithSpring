09769425784882ec5153faceb39ceda3024e03f3
Verónica Guzmám <120152801+GuzmanVero@users.noreply.github.com>
2023-11-02 15:50:38 -0600

    add request for save and get

--- backend-bbq-place/src/main/java/com/unicaes/TheBBQPlace/web/controller/ClientController.java
+++ backend-bbq-place/src/main/java/com/unicaes/TheBBQPlace/web/controller/ClientController.java
@@ -1,4 +1,23 @@
 package com.unicaes.TheBBQPlace.web.controller;
 
+import com.unicaes.TheBBQPlace.domain.service.ClientService;
+import com.unicaes.TheBBQPlace.persistence.entity.ClientEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
 public class ClientController {
+    @Autowired
+    private ClientService clientService;
+
+    @PostMapping("/registro")
+    public ClientEntity save(@RequestBody ClientEntity client){
+        return clientService.save(client);
+    }
+
+    @GetMapping("/{id}")
+    public ClientEntity getId(@PathVariable("id") int clientId) {
+        return clientService.getId(clientId);
+    }
 }
