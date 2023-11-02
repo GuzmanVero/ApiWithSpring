67492a7930300a0d5afbc4f169ec6442e8ebe51c
Verónica Guzmám <120152801+GuzmanVero@users.noreply.github.com>
2023-11-02 15:47:46 -0600

    add new find

--- backend-bbq-place/src/main/java/com/unicaes/TheBBQPlace/persistence/crud/ClientCrudRepository.java
+++ backend-bbq-place/src/main/java/com/unicaes/TheBBQPlace/persistence/crud/ClientCrudRepository.java
@@ -1,12 +1,14 @@
 package com.unicaes.TheBBQPlace.persistence.crud;
 
-import com.unicaes.TheBBQPlace.persistence.entity.ClienteEntity;
+import com.unicaes.TheBBQPlace.persistence.entity.ClientEntity;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface ClienteCrudRepository extends CrudRepository<ClienteEntity, Integer> {
-    //@Query(value = "SELECT * FROM cliente WHERE id_cliente = ?", nativeQuery = true) FORMA NATIVA
-    List<ClienteEntity> findByIdClienteOrderByNombreAsc(int idCliente);
+public interface ClientCrudRepository extends CrudRepository<ClientEntity, Integer> {
+    
+    ClientEntity findById(int clientId);
+
+    List<ClientEntity> findByIdClienteOrderByNombreAsc(int clientId);
 
 }
