02f6ebd9d0840bb7a6234949f6735dc62dd7bab1
Verónica Guzmám <120152801+GuzmanVero@users.noreply.github.com>
2023-11-16 20:57:40 -0600

    modify and add idpayment

--- backend-bbq-place/src/main/java/com/unicaes/TheBBQPlace/persistence/entity/TableEntity.java
+++ backend-bbq-place/src/main/java/com/unicaes/TheBBQPlace/persistence/entity/TableEntity.java
@@ -4,22 +4,36 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+import java.time.LocalTime;
+
 @Entity
-@Table(name = "mesas")
+@Table(name = "reservaciones")
 @Getter
 @Setter
-public class TableEntity {
+public class ReserveEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_reservacion")
+    private Integer reserveId;
+
+    @Column(name = "comentarios")
+    private Integer comments;
+
+    @Column(name = "fecha_reserva")
+    private Date date;
+
+    @Column(name = "hora_reserva")
+    private LocalTime hour;
+
     @Column(name = "id_mesa")
     private Integer tableId;
 
-    @Column(name = "nombre_mesa")
-    private String name;
+    @Column(name = "id_menu")
+    private String idMenu;
 
-    @Column(name = "capacidad")
-    private Integer capacity;
+    @Column(name = "id_pago")
+    private String idPayment;
 
-    @Column(name = "id_cliente")
-    private  Integer idClient;
 }
